f1735c57dbe125faafbd33f1deb114acd69a3487
Tarun Janapati <63157544+74run@users.noreply.github.com>
2023-09-01 15:43:41 -0400

    Update PT.js

--- static/PT.js
+++ static/PT.js
@@ -408,73 +408,435 @@ drynessUnitSelect3.addEventListener("change", function() {
 
 
 
+function attachUnitConverterListener(selectSelector, inputName, conversionFn) {
+  document.querySelector(selectSelector).addEventListener('change', function () {
+    let input = document.querySelector(`input[name="${inputName}"]`);
+    let selectedUnit = this.value;
+    let currentValue = parseFloat(input.value);
 
+    // Call the provided conversion function
+    input.value = conversionFn(currentValue, selectedUnit);
+  });
+}
 
-document.querySelector('#temperature-units').addEventListener('change', function () {
-  let temperatureInput = document.querySelector('input[name="saturation"]');
-  let selectedUnit = this.value;
-  let currentValue = parseFloat(temperatureInput.value);
-
-  if (selectedUnit === 'F') {
-    temperatureInput.value = ((currentValue * 9/5) + 32).toFixed(2);
+// Define your conversion functions
+function temperatureConversion(value, unit) {
+  if (unit === 'F') {
+	return ((value * 9/5) + 32).toFixed(2);
+    
   } else {
-    temperatureInput.value = ((currentValue - 32) * 5/9).toFixed(2);
+	return ((value - 32) * 5/9).toFixed(2); 
   }
-});
-document.querySelector('#stemperature-units').addEventListener('change', function () {
-  let temperatureInput = document.querySelector('input[name="stemp"]');
-  let selectedUnit = this.value;
-  let currentValue = parseFloat(temperatureInput.value);
-  if (selectedUnit === 'F') {
-    temperatureInput.value = ((currentValue*9/5) + 32).toFixed(2);
-  } else {
-    temperatureInput.value = ((currentValue - 32)*5/9).toFixed(2);
-  }
-});
-document.querySelector('#internal-units').addEventListener('change', function () {
-  let temperatureInput = document.querySelector('input[name="internal"]');
-  let selectedUnit = this.value;
-  let currentValue = parseFloat(temperatureInput.value);
+}
 
-  if (selectedUnit === 'F') {
-    temperatureInput.value = (currentValue)*1000;
-  } else {
-    temperatureInput.value = (currentValue)/1000;
-  }
-});
-document.querySelector('#senthalpy-units').addEventListener('change', function () {
-  let temperatureInput = document.querySelector('input[name="senthalpy"]');
-  let selectedUnit = this.value;
-  let currentValue = parseFloat(temperatureInput.value);
 
-  if (selectedUnit === 'F') {
-    temperatureInput.value = (currentValue)*1000;
-  } else {
-    temperatureInput.value = (currentValue)/1000;
-  }
-});
-document.querySelector('#sentropy-units').addEventListener('change', function () {
-  let temperatureInput = document.querySelector('input[name="sentropy"]');
-  let selectedUnit = this.value;
-  let currentValue = parseFloat(temperatureInput.value);
 
-  if (selectedUnit === 'F') {
-    temperatureInput.value = (currentValue)*1000;
+function KiloJtoJ(value, unit) {
+  if (unit === 'F') {
+    return value * 1000;
   } else {
-    temperatureInput.value = (currentValue)/1000;
+    return value / 1000;
   }
-});
-document.querySelector("#sdry-units").addEventListener("change", function() {
-  let sdryInput = document.querySelector("input[name='sdry']");
-  let selectedUnit = this.value;
-  let currentValue = parseFloat(sdryInput.value);
+}
 
-  if (selectedUnit === 'F') {
-    sdryInput.value = (currentValue)*100;
+function fractoper(value, unit) {
+  if (unit === 'F') {
+    return value * 100;
   } else {
-    sdryInput.value = (currentValue)/100;
+    return value / 100;
   }
-});
+}
+
+
+  // Function to attach event listener for temperature dropdown
+function attachTemperatureListener() {
+attachUnitConverterListener('#saturation-ur1', 'saturation-r1', temperatureConversion);
+}
+
+function attachSTemperatureListener() {
+attachUnitConverterListener('#stemp-ur2', 'stemp-r2', temperatureConversion);
+}
+
+// Function to attach event listener for energy dropdown
+function attachEnergyListener() {
+attachUnitConverterListener('#internal-ur1', 'internal-r1', KiloJtoJ);
+}
+
+function attachEnthalpyListener() {
+attachUnitConverterListener('#senthalpy-ur1', 'senthalpy-r1', KiloJtoJ);
+}
+
+function attachEntropyListener() {
+attachUnitConverterListener('#sentropy-ur1', 'sentropy-r1', KiloJtoJ);
+}
+
+
+function attachDrynessListener() {
+	attachUnitConverterListener('#dry-ur1', 'dry-r1', fractoper);
+}
+	
+
+ document.addEventListener('DOMContentLoaded', function () {
+    // Call the functions to attach event listeners
+	 attachTemperatureListener();
+     attachEnergyListener();
+	 attachEnthalpyListener();
+	 attachEntropyListener();
+	 attachDrynessListener();
+
+  });
+
+//second//
+
+function attachTemperatureListener2() {
+attachUnitConverterListener('#saturation-ur2', 'saturation-r2', temperatureConversion);
+}
+
+function attachSTemperatureListener2() {
+attachUnitConverterListener('#stemp-ur2', 'stemp-r2', temperatureConversion);
+}
+
+// Function to attach event listener for energy dropdown
+function attachEnergyListener2() {
+attachUnitConverterListener('#internal-ur2', 'internal-r2', KiloJtoJ);
+}
+
+function attachEnthalpyListener2() {
+attachUnitConverterListener('#senthalpy-ur2', 'senthalpy-r2', KiloJtoJ);
+}
+
+function attachDrynessListener2() {
+	attachUnitConverterListener('#dry-ur2', 'dry-r2', fractoper);
+}
+
+
+
+// Attach listeners using the appropriate conversion functions
+ document.addEventListener('DOMContentLoaded', function () {
+    // Call the functions to attach event listeners
+	 attachTemperatureListener2();
+     attachEnergyListener2();
+	 attachSTemperatureListener2();
+	 attachEnthalpyListener2();
+	 attachDrynessListener2();
+
+  });
+
+//third//
+
+function attachTemperatureListener3() {
+attachUnitConverterListener('#saturation-ur3', 'saturation-r3', temperatureConversion);
+}
+
+function attachSTemperatureListener3() {
+attachUnitConverterListener('#stemp-ur3', 'stemp-r3', temperatureConversion);
+}
+
+// Function to attach event listener for energy dropdown
+function attachEnergyListener3() {
+attachUnitConverterListener('#internal-ur3', 'internal-r3', KiloJtoJ);
+}
+
+function attachEntropyListener3() {
+attachUnitConverterListener('#sentropy-ur3', 'sentropy-r3', KiloJtoJ);
+}
+
+function attachDrynessListener3() {
+	attachUnitConverterListener('#dry-ur3', 'dry-r3', fractoper);
+}
+
+
+
+// Attach listeners using the appropriate conversion functions
+ document.addEventListener('DOMContentLoaded', function () {
+    // Call the functions to attach event listeners
+	 attachTemperatureListener3();
+     attachEnergyListener3();
+	 attachSTemperatureListener3();
+	 attachEntropyListener3();
+	 attachDrynessListener3();
+
+  });
+
+//fourth//
+
+function attachTemperatureListener4() {
+attachUnitConverterListener('#saturation-ur4', 'saturation-r4', temperatureConversion);
+}
+
+function attachSTemperatureListener4() {
+attachUnitConverterListener('#stemp-ur4', 'stemp-r4', temperatureConversion);
+}
+
+// Function to attach event listener for energy dropdown
+function attachEnergyListener4() {
+attachUnitConverterListener('#internal-ur4', 'internal-r4', KiloJtoJ);
+}
+
+function attachEnthalpyListener4() {
+attachUnitConverterListener('#senthalpy-ur4', 'senthalpy-r4', KiloJtoJ);
+}
+
+function attachEntropyListener4() {
+attachUnitConverterListener('#sentropy-ur4', 'sentropy-r4', KiloJtoJ);
+}
+
+
+ document.addEventListener('DOMContentLoaded', function () {
+    // Call the functions to attach event listeners
+	 attachTemperatureListener4();
+     attachEnergyListener4();
+	 attachEnthalpyListener4();
+	 attachEntropyListener4();
+
+  });
+
+//fifth//
+
+
+function pressureConversion(value, unit) {
+  if (unit === 'F') {
+    return value * 10;
+  } else {
+    return value / 10;
+  }
+}
+
+
+
+function attachPressureListener5() {
+attachUnitConverterListener('#saturationP-ur5', 'saturationP-r5', pressureConversion);
+}
+
+function attachSPressureListener5() {
+attachUnitConverterListener('#sP-ur5', 'sP-r5', pressureConversion);
+}
+
+// Function to attach event listener for energy dropdown
+function attachEnergyListener5() {
+attachUnitConverterListener('#internal-ur5', 'internal-r5', KiloJtoJ);
+}
+
+function attachEnthalpyListener5() {
+attachUnitConverterListener('#senthalpy-ur5', 'senthalpy-r5', KiloJtoJ);
+}
+
+function attachDrynessListener5() {
+	attachUnitConverterListener('#dry-ur5', 'dry-r5', fractoper);
+}
+
+
+
+// Attach listeners using the appropriate conversion functions
+ document.addEventListener('DOMContentLoaded', function () {
+    // Call the functions to attach event listeners
+	 attachPressureListener5();
+     attachEnergyListener5();
+	 attachSPressureListener5();
+	 attachEnthalpyListener5();
+	 attachDrynessListener5();
+
+  });
+
+
+//sixth//
+
+function attachPressureListener6() {
+attachUnitConverterListener('#saturationP-ur6', 'saturationP-r6', pressureConversion);
+}
+
+function attachSPressureListener6() {
+attachUnitConverterListener('#sP-ur6', 'sP-r6', pressureConversion);
+}
+
+// Function to attach event listener for energy dropdown
+function attachEnergyListener6() {
+attachUnitConverterListener('#internal-ur6', 'internal-r6', KiloJtoJ);
+}
+
+function attachEntropyListener6() {
+attachUnitConverterListener('#sentropy-ur6', 'sentropy-r6', KiloJtoJ);
+}
+
+function attachDrynessListener6() {
+	attachUnitConverterListener('#dry-ur6', 'dry-r6', fractoper);
+}
+
+
+
+// Attach listeners using the appropriate conversion functions
+ document.addEventListener('DOMContentLoaded', function () {
+    // Call the functions to attach event listeners
+	 attachPressureListener6();
+     attachEnergyListener6();
+	 attachSPressureListener6();
+	 attachEntropyListener6();
+	 attachDrynessListener6();
+
+  });
+
+
+//seventh//
+
+function attachPressureListener7() {
+attachUnitConverterListener('#saturationP-ur7', 'saturationP-r7', pressureConversion);
+}
+
+
+// Function to attach event listener for energy dropdown
+function attachEnergyListener7() {
+attachUnitConverterListener('#internal-ur7', 'internal-r7', KiloJtoJ);
+}
+
+function attachEnthalpyListener7() {
+attachUnitConverterListener('#senthalpy-ur7', 'senthalpy-r7', KiloJtoJ);
+}
+
+function attachEntropyListener7() {
+attachUnitConverterListener('#sentropy-ur7', 'sentropy-r7', KiloJtoJ);
+}
+
+
+
+
+// Attach listeners using the appropriate conversion functions
+ document.addEventListener('DOMContentLoaded', function () {
+    // Call the functions to attach event listeners
+	 attachPressureListener7();
+     attachEnergyListener7();
+	 attachEnthalpyListener7();
+	 attachEntropyListener7();
+	 
+
+  });
+
+//eighth//
+
+function attachPressureListener8() {
+attachUnitConverterListener('#saturationP-ur8', 'saturationP-r8', pressureConversion);
+}
+
+function attachTemperatureListener8() {
+attachUnitConverterListener('#stemp-ur8', 'stemp-r8', temperatureConversion);
+}
+
+// Function to attach event listener for energy dropdown
+function attachEnergyListener8() {
+attachUnitConverterListener('#internal-ur8', 'internal-r8', KiloJtoJ);
+}
+
+function attachDrynessListener8() {
+	attachUnitConverterListener('#dry-ur8', 'dry-r8', fractoper);
+}
+
+
+
+// Attach listeners using the appropriate conversion functions
+ document.addEventListener('DOMContentLoaded', function () {
+    // Call the functions to attach event listeners
+	 attachPressureListener8();
+	 attachTemperatureListener8();
+	 attachEnergyListener8();
+	 attachDrynessListener8();
+
+  });
+
+//ninth//
+
+
+function attachPressureListener9() {
+attachUnitConverterListener('#spressure-ur9', 'spressure-r9', pressureConversion);
+}
+
+function attachTemperatureListener9() {
+attachUnitConverterListener('#saturation-ur9', 'saturation-r9', temperatureConversion);
+}
+
+// Function to attach event listener for energy dropdown
+function attachEntropyLListener9() {
+attachUnitConverterListener('#entropyL-ur9', 'entropyL-r9', KiloJtoJ);
+}
+
+function attachEntropyVListener9() {
+attachUnitConverterListener('#entropyV-ur9', 'entropyV-r9', KiloJtoJ);
+}
+
+function attachEntropyListener9() {
+attachUnitConverterListener('#sentropy-ur9', 'sentropy-r9', KiloJtoJ);
+}
+
+function attachEnthalpyLListener9() {
+attachUnitConverterListener('#enthalpyL-ur9', 'enthalpyL-r9', KiloJtoJ);
+}
+
+
+function attachEnthalpyVListener9() {
+attachUnitConverterListener('#enthalpyV-ur9', 'enthalpyV-r9', KiloJtoJ);
+}
+
+
+// Attach listeners using the appropriate conversion functions
+ document.addEventListener('DOMContentLoaded', function () {
+    // Call the functions to attach event listeners
+	 attachTemperatureListener9();
+	 attachPressureListener9();
+	 attachEntropyLListener9();
+	 attachEntropyVListener9();
+	 attachEntropyListener9();
+	 attachEnthalpyLListener9();
+	 attachEnthalpyVListener9();
+
+  });
+
+//tenth//
+
+
+function attachPressureListener10() {
+attachUnitConverterListener('#spressure-ur10', 'spressure-r10', pressureConversion);
+}
+
+function attachTemperatureListener10() {
+attachUnitConverterListener('#saturation-ur10', 'saturation-r10', temperatureConversion);
+}
+
+// Function to attach event listener for energy dropdown
+function attachEntropyLListener10() {
+attachUnitConverterListener('#entropyL-ur10', 'entropyL-r10', KiloJtoJ);
+}
+
+function attachEntropyVListener10() {
+attachUnitConverterListener('#entropyV-ur10', 'entropyV-r10', KiloJtoJ);
+}
+
+function attachEnthalpyListener10() {
+attachUnitConverterListener('#enthalpy-ur10', 'enthalpy-r10', KiloJtoJ);
+}
+
+function attachEnthalpyLListener10() {
+attachUnitConverterListener('#enthalpyL-ur10', 'enthalpyL-r10', KiloJtoJ);
+}
+
+
+function attachEnthalpyVListener10() {
+attachUnitConverterListener('#enthalpyV-ur10', 'enthalpyV-r10', KiloJtoJ);
+}
+
+
+// Attach listeners using the appropriate conversion functions
+ document.addEventListener('DOMContentLoaded', function () {
+    // Call the functions to attach event listeners
+	 attachTemperatureListener10();
+	 attachPressureListener10();
+	 attachEntropyLListener10();
+	 attachEntropyVListener10();
+	 attachEnthalpyListener10();
+	 attachEnthalpyLListener10();
+	 attachEnthalpyVListener10();
+
+  });
+
+
+
 
 
 
